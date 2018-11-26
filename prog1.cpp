c51a58269cdfcfbaf1940db09dfa91bcfc3408bf
Natalie Rose <nnrose123@yahoo.com>
2018-11-25 21:17:41 -0500

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main ()
+
+{
+  char str[] = "I have too much work";
+  char * ptr = &str[0];
+  ptr = ptr + 3;
+  cout<< ptr[4];
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
